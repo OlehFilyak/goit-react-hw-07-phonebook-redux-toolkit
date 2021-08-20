7eb83aca1cd9c789d174a233bf9f59d77fdee2f8
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-08-20 15:06:31 +0300

    add logic for mockapi

--- src/redux/slices/contacts.js
+++ src/redux/slices/contacts.js
@@ -4,7 +4,9 @@ import axios from "axios";
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchContacts",
   async () => {
-    const result = await axios.get("http://localhost:7777/contacts");
+    const result = await axios.get(
+      "https://611f63ca9771bf001785c8de.mockapi.io/contacts"
+    );
     return result.data;
   }
 );
@@ -12,7 +14,10 @@ export const fetchContacts = createAsyncThunk(
 export const addContact = createAsyncThunk(
   "contacts/setContact",
   async (contact) => {
-    const result = await axios.post("http://localhost:7777/contacts", contact);
+    const result = await axios.post(
+      "https://611f63ca9771bf001785c8de.mockapi.io/contacts",
+      contact
+    );
     return result.data;
   }
 );
@@ -20,7 +25,9 @@ export const addContact = createAsyncThunk(
 export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
   async (id) => {
-    await axios.delete(`http://localhost:7777/contacts/${id}`);
+    await axios.delete(
+      `https://611f63ca9771bf001785c8de.mockapi.io/contacts/${id}`
+    );
     return id;
   }
 );
